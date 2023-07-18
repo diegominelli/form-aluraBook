c57a2ada00abd672e851c876f6ba209bfcc49a09
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-07-18 19:53:50 -0300

    tratando os erros

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
   .then(response => response.json())
-  .then(r => console.log(r));
+  .then(r => {
+    if (r.erro) {
+      throw Error('CEP inválido')
+    } else {
+      console.log(r)
+    }
+  })
+  .catch(erro => console.log(erro));
 
 console.log(consultaCEP);
